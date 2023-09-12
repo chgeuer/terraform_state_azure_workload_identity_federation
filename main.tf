423536e31e4330292a8eb4036a5c70928902fae5
Dr. Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2023-09-12 09:54:02 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+# https://github.com/chgeuer/terraform_state_azure_workload_identity_federation
+# https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/guides/service_principal_oidc
+
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=2.23.0"
+    }
+  }
+}
+
+provider "azuread" {
+  use_oidc = true
+  tenant_id = "5f9e748d-300b-48f1-85f5-3aa96d6260cb"
+  client_id = "fdf939e2-e28a-4c33-a761-efa219ce9cc0"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    use_oidc             = true
+    tenant_id            = "5f9e748d-300b-48f1-85f5-3aa96d6260cb"
+    client_id            = "fdf939e2-e28a-4c33-a761-efa219ce9cc0"
+    subscription_id      = "706df49f-998b-40ec-aed3-7f0ce9c67759"
+    resource_group_name  = "tfstaterg"
+    storage_account_name = "tftestchgp123"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
