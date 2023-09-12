0039951ea18927293bce505ef42f2497bf70ac42
Dr. Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2023-09-12 14:33:05 +0200

    a

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 # https://github.com/chgeuer/terraform_state_azure_workload_identity_federation
 # https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/guides/service_principal_oidc
+# https://learn.microsoft.com/en-us/azure/developer/terraform/store-state-in-azure-storage?tabs=azure-cli
 
 terraform {
   required_providers {
@@ -15,29 +16,30 @@ terraform {
     client_id            = "fdf939e2-e28a-4c33-a761-efa219ce9cc0"
     subscription_id      = "706df49f-998b-40ec-aed3-7f0ce9c67759"
     resource_group_name  = "tfstaterg"
-    storage_account_name = "tftestchgp123"
+    storage_account_name = "tfstatechgp"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
 }
 
 provider "azuread" {
-  use_oidc = true
-  tenant_id = "5f9e748d-300b-48f1-85f5-3aa96d6260cb"
-  client_id = "fdf939e2-e28a-4c33-a761-efa219ce9cc0"
+  use_oidc               = true
+  tenant_id              = "5f9e748d-300b-48f1-85f5-3aa96d6260cb"
+  client_id              = "fdf939e2-e28a-4c33-a761-efa219ce9cc0"
   features {}
 }
 
 provider "azurerm" {
-  tenant_id         = "5f9e748d-300b-48f1-85f5-3aa96d6260cb"
-  client_id         = "fdf939e2-e28a-4c33-a761-efa219ce9cc0"
-  subscription_id   = "706df49f-998b-40ec-aed3-7f0ce9c67759"
+  tenant_id              = "5f9e748d-300b-48f1-85f5-3aa96d6260cb"
+  client_id              = "fdf939e2-e28a-4c33-a761-efa219ce9cc0"
+  subscription_id        = "706df49f-998b-40ec-aed3-7f0ce9c67759"
   features {}
 }
 
-# https://learn.microsoft.com/en-us/azure/developer/terraform/store-state-in-azure-storage?tabs=azure-cli
-
-resource "azurerm_resource_group" "state-demo-secure" {
-  name     = "state-demo"
-  location = "westeurope"
+resource "azurerm_storage_account" "example" {
+  name                     = "stchgp123tf"
+  resource_group_name      = "tfstaterg"
+  location                 = "westeurope"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
